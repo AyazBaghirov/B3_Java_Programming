c7fb3337a51b3ad4f79da85e33bbbc7d81596ef7
Admin <ayazbagirov89@gmail.com>
2024-04-27 22:40:55 +0400

    new topic for loop

---
+++ src/day18_loops/CountHi.java
@@ -0,0 +1,19 @@
+package day18_loops;
+
+public class CountHi {
+    public static void main(String[] args) {
+        String str = "aaahiahhihibbbcch";//hi
+        int count =0;
+
+        for (int index =0; index<str.length()-1; index++){
+
+          if(str.charAt(index)=='h' && str.charAt(index+1)=='i'){
+              count++;
+
+          }
+
+        }
+        System.out.println(count);
+
+    }
+}
