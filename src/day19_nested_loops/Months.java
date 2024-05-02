718df96639a93f6a041b47be9a4757204fd9777f
Admin <ayazbagirov89@gmail.com>
2024-05-02 06:14:46 +0400

    nested loop

---
+++ src/day19_nested_loops/Months.java
@@ -0,0 +1,18 @@
+package day19_nested_loops;
+/*
+       Go through a whole month, by looping through each week and each day
+        */
+public class Months {
+    public static void main(String[] args) {
+
+
+        for (int week = 0; week <=4 ; week++) {
+            System.out.println("Week: " + week);
+            for (int day = 0; day <=7; day++) {
+                System.out.println("\tDay: " + day);
+
+            }
+
+        }
+    }
+}
