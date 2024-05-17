295b52fb9518c2ccd8e9f11fa8825f5b81d6889c
Admin <ayazbagirov89@gmail.com>
2024-05-17 05:58:53 +0400

    method overloading, var-args, type promotion

---
+++ src/day26_methods/painter.java
@@ -0,0 +1,15 @@
+package day26_methods;
+
+public class painter {
+    public static void main(String[] args) {
+       Picture.draw();
+       Picture.draw("yellow");
+       Picture.draw("green","red");
+       Picture.draw(4);
+       Picture.draw(4,"pink");
+       //Picture.draw(true);
+       Picture.draw("hello");
+       Picture.draw(1);
+
+    }
+}
