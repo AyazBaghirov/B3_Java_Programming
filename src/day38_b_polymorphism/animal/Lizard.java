79791ae30d83756c42f0900d7e7711547be01c02
Admin <ayazbagirov89@gmail.com>
2024-06-15 23:02:38 +0400

    casting, instanceof, polymorphic array

--- src/day38_b_polymorphism/animal/Lizard.java
+++ src/day38_b_polymorphism/animal/Lizard.java
@@ -1,7 +1,7 @@
 package day38_b_polymorphism.animal;
 
-public class Lizard extends Animal {
-    String skilColor;
+public class Lizard extends Reptile {
+    String skinColor;
 
     @Override
     public void eat(){
